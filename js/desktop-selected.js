5470b267a9a5a7f9306174716649e69401b3caaa
Tais de Souza Lessa <tais@mozillafoundation.org>
2017-05-25 14:57:14 -0700

    default desktop

--- js/desktop-selected.js
+++ js/desktop-selected.js
@@ -1,7 +1,7 @@
 $(function(){
+	$("a.about-proj").css('border-bottom', 'solid 5px #FFED00');
   
   $(".desktop-menu li a").click(function(){
-
   	$(".desktop-menu li h2 a").css('border-bottom', 'solid 0px white');
      $(this).css('border-bottom', 'solid 5px #FFED00');
 
